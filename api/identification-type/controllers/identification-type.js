05762281c805cc476e31c1432a6cd0cca9332c83
Tuan <anhtuanhoang2312@gmail.com>
2020-06-30 22:44:53 +0700

    Fix bugs

--- api/identification-type/controllers/identification-type.js
+++ api/identification-type/controllers/identification-type.js
@@ -6,23 +6,5 @@
  */
 
 module.exports = {
-    async auth(ctx){
-        var userName = ctx.request.body.username;
-        var password = ctx.request.body.password;
-        var user  = await strapi.services["customer-accounts"].findOne({user_name: userName});
-        var compare = await strapi.services["customer-accounts"].verifyPassword(password,user.password);
-        if(!user || !compare)
-        {
-            ctx.status(401);
-        }
-        else
-        {
-            ctx.send({
-                status: 200,
-                result : user
-
-           });
-        }
-    }
 
 };
