264417a7fb4797a65550debef34e7da1c22cef95
zenusnguyen <zenusnguyen@gmail.com>
2020-07-05 22:26:45 +0700

    update

--- database_scripts.sql
+++ database_scripts.sql
@@ -12,7 +12,7 @@
  Target Server Version : 120003
  File Encoding         : 65001
 
- Date: 30/06/2020 21:06:18
+ Date: 05/07/2020 22:26:19
 */
 
 
@@ -50,21 +50,10 @@ START 1
 CACHE 1;
 
 -- ----------------------------
--- Sequence structure for currency_units_id_seq
+-- Sequence structure for currency_unit_id_seq
 -- ----------------------------
-DROP SEQUENCE IF EXISTS "public"."currency_units_id_seq";
-CREATE SEQUENCE "public"."currency_units_id_seq" 
-INCREMENT 1
-MINVALUE  1
-MAXVALUE 2147483647
-START 1
-CACHE 1;
-
--- ----------------------------
--- Sequence structure for customer_accounts_id_seq1
--- ----------------------------
-DROP SEQUENCE IF EXISTS "public"."customer_accounts_id_seq1";
-CREATE SEQUENCE "public"."customer_accounts_id_seq1" 
+DROP SEQUENCE IF EXISTS "public"."currency_unit_id_seq";
+CREATE SEQUENCE "public"."currency_unit_id_seq" 
 INCREMENT 1
 MINVALUE  1
 MAXVALUE 2147483647
@@ -76,17 +65,6 @@ CACHE 1;
 -- ----------------------------
 DROP SEQUENCE IF EXISTS "public"."customer_infor_id_seq";
 CREATE SEQUENCE "public"."customer_infor_id_seq" 
-INCREMENT 1
-MINVALUE  1
-MAXVALUE 2147483647
-START 1
-CACHE 1;
-
--- ----------------------------
--- Sequence structure for identification_type_id_seq
--- ----------------------------
-DROP SEQUENCE IF EXISTS "public"."identification_type_id_seq";
-CREATE SEQUENCE "public"."identification_type_id_seq" 
 INCREMENT 1
 MINVALUE  1
 MAXVALUE 2147483647
@@ -278,7 +256,9 @@ CREATE TABLE "public"."account_log" (
   "action" varchar(255) COLLATE "pg_catalog"."default",
   "content" varchar(255) COLLATE "pg_catalog"."default",
   "status" int4,
-  "account_id" int4
+  "account_id" int4,
+  "created_at" timestamptz(6),
+  "updated_at" timestamptz(6)
 )
 ;
 
@@ -288,7 +268,9 @@ CREATE TABLE "public"."account_log" (
 DROP TABLE IF EXISTS "public"."bank";
 CREATE TABLE "public"."bank" (
   "name" varchar(255) COLLATE "pg_catalog"."default",
-  "id" int4 NOT NULL DEFAULT nextval('banking_id_seq'::regclass)
+  "id" int4 NOT NULL DEFAULT nextval('banking_id_seq'::regclass),
+  "created_at" timestamptz(6),
+  "updated_at" timestamptz(6)
 )
 ;
 
@@ -311,61 +293,29 @@ CREATE TABLE "public"."core_store" (
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."currency_unit";
 CREATE TABLE "public"."currency_unit" (
+  "id" int4 NOT NULL DEFAULT nextval('currency_unit_id_seq'::regclass),
   "unit" varchar(255) COLLATE "pg_catalog"."default",
-  "id" int4 NOT NULL DEFAULT nextval('currency_units_id_seq'::regclass),
-  "tpye" varchar(255) COLLATE "pg_catalog"."default",
-  "created_at" timestamptz(6),
-  "updated_at" timestamptz(6)
-)
-;
-
--- ----------------------------
--- Table structure for customer_accounts
--- ----------------------------
-DROP TABLE IF EXISTS "public"."customer_accounts";
-CREATE TABLE "public"."customer_accounts" (
-  "id" int4 NOT NULL DEFAULT nextval('customer_accounts_id_seq1'::regclass),
-  "user_name" varchar(255) COLLATE "pg_catalog"."default",
-  "password" varchar(255) COLLATE "pg_catalog"."default",
   "created_at" timestamptz(6) DEFAULT CURRENT_TIMESTAMP,
   "updated_at" timestamptz(6) DEFAULT CURRENT_TIMESTAMP
 )
 ;
 
 -- ----------------------------
--- Table structure for customer_info
+-- Table structure for customer_infor
 -- ----------------------------
-DROP TABLE IF EXISTS "public"."customer_info";
-CREATE TABLE "public"."customer_info" (
-  "first_name" varchar(255) COLLATE "pg_catalog"."default",
-  "last_name" varchar(255) COLLATE "pg_catalog"."default",
-  "email" varchar(255) COLLATE "pg_catalog"."default",
+DROP TABLE IF EXISTS "public"."customer_infor";
+CREATE TABLE "public"."customer_infor" (
+  "full_name" varchar(255) COLLATE "pg_catalog"."default",
   "phone_number" varchar(255) COLLATE "pg_catalog"."default",
   "address" varchar(32) COLLATE "pg_catalog"."default",
-  "identification_id" int4,
   "id" int4 NOT NULL DEFAULT nextval('customer_infor_id_seq'::regclass),
-  "date_of_birth" date
-)
-;
-
--- ----------------------------
--- Table structure for identification
--- ----------------------------
-DROP TABLE IF EXISTS "public"."identification";
-CREATE TABLE "public"."identification" (
-  "id" int4 NOT NULL,
-  "identification_type" int4,
-  "identification_detail" int4
-)
-;
-
--- ----------------------------
--- Table structure for identification_type
--- ----------------------------
-DROP TABLE IF EXISTS "public"."identification_type";
-CREATE TABLE "public"."identification_type" (
-  "type" varchar(255) COLLATE "pg_catalog"."default",
-  "id" int4 NOT NULL DEFAULT nextval('identification_type_id_seq'::regclass)
+  "date_of_birth" date,
+  "created_at" timestamptz(6),
+  "updated_at" timestamptz(6),
+  "identificationNumber" varchar(32) COLLATE "pg_catalog"."default",
+  "date_of_issue" date,
+  "img1" text COLLATE "pg_catalog"."default",
+  "img2" text COLLATE "pg_catalog"."default"
 )
 ;
 
@@ -374,13 +324,13 @@ CREATE TABLE "public"."identification_type" (
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."identity_card";
 CREATE TABLE "public"."identity_card" (
+  "id" int4 NOT NULL DEFAULT nextval('identity_card_id_seq'::regclass),
+  "code" varchar(255) COLLATE "pg_catalog"."default",
   "full_name" varchar(255) COLLATE "pg_catalog"."default",
   "date_of_issue" date,
-  "code" varchar(255) COLLATE "pg_catalog"."default",
-  "image1" text COLLATE "pg_catalog"."default",
-  "image2" text COLLATE "pg_catalog"."default",
-  "id" int4 NOT NULL DEFAULT nextval('identity_card_id_seq'::regclass),
-  "type" int4
+  "type" int4,
+  "image1" varchar(255) COLLATE "pg_catalog"."default",
+  "image2" varchar(255) COLLATE "pg_catalog"."default"
 )
 ;
 
@@ -391,7 +341,8 @@ DROP TABLE IF EXISTS "public"."interest_rate";
 CREATE TABLE "public"."interest_rate" (
   "period" int4,
   "interst_rate" float4,
-  "id" int4 NOT NULL DEFAULT nextval('interest_rate_id_seq'::regclass)
+  "id" int4 NOT NULL DEFAULT nextval('interest_rate_id_seq'::regclass),
+  "interest_rate" float8
 )
 ;
 
@@ -401,14 +352,15 @@ CREATE TABLE "public"."interest_rate" (
 DROP TABLE IF EXISTS "public"."spend_account";
 CREATE TABLE "public"."spend_account" (
   "account_id" int4,
-  "currency_unit_id" int4,
+  "currency_unit" varchar(255) COLLATE "pg_catalog"."default",
   "balance" float8,
   "closed_date" date,
-  "status" int4,
+  "status" varchar(255) COLLATE "pg_catalog"."default",
   "id" int4 NOT NULL DEFAULT nextval('spend_account_infor_id_seq'::regclass),
   "type" int4,
   "card_number" varchar(32) COLLATE "pg_catalog"."default",
-  "term_deposit_id" int4
+  "term_deposit_id" int4,
+  "created_date" date
 )
 ;
 
@@ -494,7 +446,9 @@ CREATE TABLE "public"."transaction_log" (
 DROP TABLE IF EXISTS "public"."transaction_type";
 CREATE TABLE "public"."transaction_type" (
   "id" int4 NOT NULL DEFAULT nextval('transaction_types_id_seq'::regclass),
-  "type" varchar(255) COLLATE "pg_catalog"."default"
+  "type" varchar(255) COLLATE "pg_catalog"."default",
+  "created_at" timestamptz(6),
+  "updated_at" timestamptz(6)
 )
 ;
 
@@ -506,7 +460,9 @@ CREATE TABLE "public"."transfer_log" (
   "beneficiary_account" varchar(255) COLLATE "pg_catalog"."default",
   "remark" varchar(255) COLLATE "pg_catalog"."default",
   "beneficiary_bank" varchar(255) COLLATE "pg_catalog"."default",
-  "id" int4 NOT NULL DEFAULT nextval('transfer_logs_id_seq'::regclass)
+  "id" int4 NOT NULL DEFAULT nextval('transfer_logs_id_seq'::regclass),
+  "created_at" timestamptz(6),
+  "updated_at" timestamptz(6)
 )
 ;
 
@@ -593,7 +549,7 @@ CREATE TABLE "public"."users-permissions_user" (
   "created_at" timestamptz(6) DEFAULT CURRENT_TIMESTAMP,
   "updated_at" timestamptz(6) DEFAULT CURRENT_TIMESTAMP,
   "status" int4,
-  "user_infor" int4
+  "user_info" int4
 )
 ;
 
@@ -608,22 +564,16 @@ OWNED BY "public"."bank"."id";
 SELECT setval('"public"."banking_id_seq"', 5, false);
 ALTER SEQUENCE "public"."core_store_id_seq"
 OWNED BY "public"."core_store"."id";
-SELECT setval('"public"."core_store_id_seq"', 22, true);
-ALTER SEQUENCE "public"."currency_units_id_seq"
+SELECT setval('"public"."core_store_id_seq"', 45, true);
+ALTER SEQUENCE "public"."currency_unit_id_seq"
 OWNED BY "public"."currency_unit"."id";
-SELECT setval('"public"."currency_units_id_seq"', 4, false);
-ALTER SEQUENCE "public"."customer_accounts_id_seq1"
-OWNED BY "public"."customer_accounts"."id";
-SELECT setval('"public"."customer_accounts_id_seq1"', 2, true);
+SELECT setval('"public"."currency_unit_id_seq"', 2, false);
 ALTER SEQUENCE "public"."customer_infor_id_seq"
-OWNED BY "public"."customer_info"."id";
-SELECT setval('"public"."customer_infor_id_seq"', 5, false);
-ALTER SEQUENCE "public"."identification_type_id_seq"
-OWNED BY "public"."identification_type"."id";
-SELECT setval('"public"."identification_type_id_seq"', 5, false);
+OWNED BY "public"."customer_infor"."id";
+SELECT setval('"public"."customer_infor_id_seq"', 28, true);
 ALTER SEQUENCE "public"."identity_card_id_seq"
 OWNED BY "public"."identity_card"."id";
-SELECT setval('"public"."identity_card_id_seq"', 5, false);
+SELECT setval('"public"."identity_card_id_seq"', 2, false);
 ALTER SEQUENCE "public"."interest_rate_id_seq"
 OWNED BY "public"."interest_rate"."id";
 SELECT setval('"public"."interest_rate_id_seq"', 5, false);
@@ -632,7 +582,7 @@ OWNED BY "public"."term_deposit"."id";
 SELECT setval('"public"."saving_account_infor_id_seq"', 5, false);
 ALTER SEQUENCE "public"."spend_account_infor_id_seq"
 OWNED BY "public"."spend_account"."id";
-SELECT setval('"public"."spend_account_infor_id_seq"', 4, false);
+SELECT setval('"public"."spend_account_infor_id_seq"', 15, true);
 ALTER SEQUENCE "public"."spend_account_logs_id_seq"
 OWNED BY "public"."transaction_log"."id";
 SELECT setval('"public"."spend_account_logs_id_seq"', 4, false);
@@ -656,19 +606,19 @@ OWNED BY "public"."transfer_log"."id";
 SELECT setval('"public"."transfer_logs_id_seq"', 4, false);
 ALTER SEQUENCE "public"."upload_file_id_seq"
 OWNED BY "public"."upload_file"."id";
-SELECT setval('"public"."upload_file_id_seq"', 2, false);
+SELECT setval('"public"."upload_file_id_seq"', 55, true);
 ALTER SEQUENCE "public"."upload_file_morph_id_seq"
 OWNED BY "public"."upload_file_morph"."id";
 SELECT setval('"public"."upload_file_morph_id_seq"', 2, false);
 ALTER SEQUENCE "public"."users-permissions_permission_id_seq"
 OWNED BY "public"."users-permissions_permission"."id";
-SELECT setval('"public"."users-permissions_permission_id_seq"', 171, true);
+SELECT setval('"public"."users-permissions_permission_id_seq"', 317, true);
 ALTER SEQUENCE "public"."users-permissions_role_id_seq"
 OWNED BY "public"."users-permissions_role"."id";
 SELECT setval('"public"."users-permissions_role_id_seq"', 13, true);
 ALTER SEQUENCE "public"."users-permissions_user_id_seq"
 OWNED BY "public"."users-permissions_user"."id";
-SELECT setval('"public"."users-permissions_user_id_seq"', 4, true);
+SELECT setval('"public"."users-permissions_user_id_seq"', 18, true);
 
 -- ----------------------------
 -- Primary Key structure for table account_log
@@ -688,27 +638,12 @@ ALTER TABLE "public"."core_store" ADD CONSTRAINT "core_store_pkey" PRIMARY KEY (
 -- ----------------------------
 -- Primary Key structure for table currency_unit
 -- ----------------------------
-ALTER TABLE "public"."currency_unit" ADD CONSTRAINT "currency_units_pkey" PRIMARY KEY ("id");
+ALTER TABLE "public"."currency_unit" ADD CONSTRAINT "currency_unit_pkey" PRIMARY KEY ("id");
 
 -- ----------------------------
--- Primary Key structure for table customer_accounts
+-- Primary Key structure for table customer_infor
 -- ----------------------------
-ALTER TABLE "public"."customer_accounts" ADD CONSTRAINT "customer_accounts_pkey1" PRIMARY KEY ("id");
-
--- ----------------------------
--- Primary Key structure for table customer_info
--- ----------------------------
-ALTER TABLE "public"."customer_info" ADD CONSTRAINT "customer_infor_pkey" PRIMARY KEY ("id");
-
--- ----------------------------
--- Primary Key structure for table identification
--- ----------------------------
-ALTER TABLE "public"."identification" ADD CONSTRAINT "customer_identification_pkey" PRIMARY KEY ("id");
-
--- ----------------------------
--- Primary Key structure for table identification_type
--- ----------------------------
-ALTER TABLE "public"."identification_type" ADD CONSTRAINT "identification_type_pkey" PRIMARY KEY ("id");
+ALTER TABLE "public"."customer_infor" ADD CONSTRAINT "customer_infor_pkey" PRIMARY KEY ("id");
 
 -- ----------------------------
 -- Primary Key structure for table identity_card
