5b45b1436b7fe9883a9e041e5dbdee250cfdd2ff
zenusnguyen <zenusnguyen@gmail.com>
2020-07-05 20:34:19 +0700

    update

--- api/spend-account/controllers/spend-account.js
+++ api/spend-account/controllers/spend-account.js
@@ -1,15 +1,24 @@
-'use strict';
+"use strict";
 // const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 
 module.exports = {
-//   async create(ctx) {
-//     let entity;
-//     if (ctx.is("multipart")) {
-//       const { data, files } = parseMultipartData(ctx);
-//       entity = await strapi.services['spend-account'].create(data, { files });
-//     } else {
-//       entity = await strapi.services['spend-account'].create(ctx.request.body);
-//     }
-//     return sanitizeEntity(entity, { model: strapi.models['spend-account'] });
-//   },
+  //   async create(ctx) {
+  //     let entity;
+  //     if (ctx.is("multipart")) {
+  //       const { data, files } = parseMultipartData(ctx);
+  //       entity = await strapi.services['spend-account'].create(data, { files });
+  //     } else {
+  //       entity = await strapi.services['spend-account'].create(ctx.request.body);
+  //     }
+  //     return sanitizeEntity(entity, { model: strapi.models['spend-account'] });
+  //   },
+  async findByAccount(ctx) {
+    console.log("ctx: ", ctx.request.body);
+    const data = await strapi
+      .query("spend-account")
+      .find({ account_id: ctx.request.body.account_id });
+
+    console.log("data: ", data);
+    return data;
+  },
 };
