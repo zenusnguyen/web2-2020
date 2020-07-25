f6c3be9024b777bc68d9c7855016b4701cd28b78
zenusnguyen <zenusnguyen@gmail.com>
2020-07-26 02:48:53 +0700

    update

--- api/spend-account/controllers/spend-account.js
+++ api/spend-account/controllers/spend-account.js
@@ -2,23 +2,54 @@
 // const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 
 module.exports = {
-  //   async create(ctx) {
-  //     let entity;
-  //     if (ctx.is("multipart")) {
-  //       const { data, files } = parseMultipartData(ctx);
-  //       entity = await strapi.services['spend-account'].create(data, { files });
-  //     } else {
-  //       entity = await strapi.services['spend-account'].create(ctx.request.body);
-  //     }
-  //     return sanitizeEntity(entity, { model: strapi.models['spend-account'] });
-  //   },
-  async findByAccount(ctx) {
-    console.log("ctx: ", ctx.request.body);
+  async findByOwner(ctx) {
+    console.log("ctx: ", ctx.query.id);
     const data = await strapi
       .query("spend-account")
-      .find({ account_id: ctx.request.body.account_id });
+      .find({ account_id: ctx.query.id });
 
-    console.log("data: ", data);
     return data;
+  },
+  async findByCardID(ctx) {
+    console.log("ctx: ", ctx.query.id);
+    const data = await strapi
+      .query("spend-account")
+      .find({ account_id: ctx.query.id });
+
+    return data;
+  },
+  async transferIntra(ctx) {
+    console.log("ctx: ", ctx.request.body);
+    const data = ctx.request.body;
+
+    const currentAccount = await strapi
+      .query("spend-account")
+      .findOne({ card_number: data.currentAccount });
+
+    const beneficiaryAccount = await strapi
+      .query("spend-account")
+      .findOne({ card_number: data.beneficiaryAccount });
+
+    await strapi
+      .query("spend-account")
+      .update(
+        { card_number: data.currentAccount },
+        { balance: parseInt(currentAccount.balance) - parseInt(data.amount) }
+      );
+
+    await strapi
+      .query("spend-account")
+      .update(
+        { card_number: data.beneficiaryAccount },
+        { balance: parseInt(beneficiaryAccount.balance) + parseInt(data.amount) }
+      );
+    const result = "asdas ";
+    console.log("result: ", result);
+    // const data = await strapi
+    //   .query("spend-account")
+    //   .find({ account_id: ctx.query.id });
+
+    // return data;
+    return result;
   },
 };
