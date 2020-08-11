a7469fe374eb2f7d9645618ae5a4ef973a47576b
zenusnguyen <zenusnguyen@gmail.com>
2020-08-11 17:40:49 +0700

    :update

--- config/functions/cron.js
+++ config/functions/cron.js
@@ -24,7 +24,7 @@ module.exports = {
   //     .find({ card_type: "saving" });
   // },
 
-  "*/1 * * * *": async () => {
+  "*0 1 * * 1": async () => {
     const RateTable = await strapi.query("interest-rate").find();
 
     const ListSaving = await strapi
@@ -53,5 +53,77 @@ module.exports = {
     });
 
     console.log("1 minute later");
+  },
+
+  // renew savingAccount
+  "*0 2 * * 1": async () => {
+    const RateTable = await strapi.query("interest-rate").find();
+
+    const ListSaving = await strapi
+      .query("spend-account")
+      .find({ card_type: "saving" });
+
+    ListSaving.forEach(async (saving) => {
+      const rate = await strapi
+        .query("term-deposit")
+        .findOne({ id: saving.term_deposit_id });
+      if (
+        rate.final_settlement_type === false &&
+        ((new Date().getFullYear === new Date(rate.maturity_date).getFullYear &&
+          new Date().getMonth === new Date(rate.maturity_date).getMonth) ||
+          new date.getDate() === new date.getDate(rate.maturity_date))
+      ) {
+        const newMaturityDate = new Date(
+          moment(rate.maturity_date).add(
+            parseInt(RateTable[rate.interest_rate_id - 1])
+          ),
+          "M"
+        );
+        const renewDate = await strapi
+          .query("term-deposit")
+          .update({ id: rate.id }, { maturity_date: newMaturityDate });
+
+        const renewOriginMoney = await strapi
+          .query("term-deposit")
+          .update({ id: rate.id }, { origin_balance: saving.balance });
+      }
+    });
+  },
+
+  // transfer to beneficiaryAccount
+
+  "*0 3 * * 1": async () => {
+    const ListSaving = await strapi
+      .query("spend-account")
+      .find({ card_type: "saving" });
+
+    ListSaving.forEach(async (saving) => {
+      const rate = await strapi
+        .query("term-deposit")
+        .findOne({ id: saving.term_deposit_id });
+      if (
+        rate.final_settlement_type === true &&
+        ((new Date().getFullYear === new Date(rate.maturity_date).getFullYear &&
+          new Date().getMonth === new Date(rate.maturity_date).getMonth) ||
+          new date.getDate() === new date.getDate(rate.maturity_date))
+      ) {
+        const beneficiaryAccount = await strapi
+          .query("spend-account")
+          .findOne({ card_number: rate.beneficiary_account });
+        const transfer = await strapi
+          .query("spend-account")
+          .update(
+            { card_number: rate.beneficiary_account },
+            { balance: beneficiaryAccount + rate.balance }
+          );
+
+        const closeAccount = await strapi
+          .query("spend-account")
+          .update(
+            { id: saving.id },
+            { status: "close", closed_date: new Date() }
+          );
+      }
+    });
   },
 };
